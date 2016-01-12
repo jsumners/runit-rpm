bdc8b35eb9dad2d68ab833578850b86642fd76fd
James Sumners <james.sumners@gmail.com>
2016-01-12 15:00:38 -0500

    Change build script

--- build.sh
+++ build.sh
@@ -1,19 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 
-whereami=$(dirname $0)
+CWD=$(pwd)
+SPEC="runit.spec"
 
-if [ ! -f "/etc/rpm/macros.dist" ] && \
-   [ ! -f "/etc/rpm/macros.disttag" ];   then echo "please install 'buildsys-macros' rpm and try again" ; exit 1 ; fi
-if [ ! -f "$(which rpmbuild)" ];         then echo "please install 'rpm-build' rpm and try again" ; exit 1 ; fi
-if [ ! -f "$(which spectool)" ];         then echo "please install 'rpmdevtools' rpm and try again" ; exit 1 ; fi
-if [ ! -f "$(which rpmdev-setuptree)" ]; then echo "please install 'rpmdevtools' rpm and try again" ; exit 1 ; fi
+which git > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Aborting. Cannot continue without git."
+  exit 1
+fi
 
-# creates ~/rpmbuild
-/usr/bin/rpmdev-setuptree
+which rpmbuild > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Aborting. Cannot continue without rpmbuild from the rpm-build package."
+  exit 1
+fi
 
-cp -f ${whereami}/runit.spec ~/rpmbuild/SPECS/
-cp -f ${whereami}/*.patch    ~/rpmbuild/SOURCES/
-cp -f ${whereami}/runsvdir-start.service    ~/rpmbuild/SOURCES/
-/usr/bin/spectool -C ~/rpmbuild/SOURCES/ -g ${whereami}/runit.spec 
+echo "Creating RPM build path structure..."
+mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS,tmp}
 
-rpmbuild -bb ~/rpmbuild/SPECS/runit.spec
+cp ${SPEC} rpmbuild/SPECS/
+cp *.patch rpmbuild/SOURCES/
+cp *.service rpmbuild/SOURCES/
+
+if [ -f ${CWD}/gpg-env ]; then
+  echo "Building RPM with GPG signing..."
+  cd ${CWD}
+
+  source gpg-env
+  if [ "${gpg_bin}" != "" ]; then
+    rpmbuild --define "_topdir ${CWD}/rpmbuild" \
+      --define "_signature ${signature}" \
+      --define "_gpg_path ${gpg_path}" --define "_gpg_name ${gpg_name}" \
+      --define "__gpg ${gpg_bin}" --sign -ba ${SPEC}
+  else
+    rpmbuild --define "_topdir ${CWD}/rpmbuild" \
+      --define "_signature ${signature}" \
+      --define "_gpg_path ${gpg_path}" --define "_gpg_name ${gpg_name}" \
+      --sign --ba ${SPEC}
+  fi
+else
+  echo "Building RPM..."
+  cd ${CWD}
+  rpmbuild --define "_topdir ${CWD}/rpmbuild" --ba ${SPEC}
+fi
+
